b2f3ab21f053ad05326363b1b90f93f796b34590
JeaneMirele <jeanemirele7@gmail.com>
2026-03-04 12:05:19 -0300

    corrige a subida do projeto

---
+++ backend/src/main/java/com/pa1/sistema_gerenciador_reserva/SistemaGerenciadorReservaApplication.java
@@ -0,0 +1,13 @@
+package com.pa1.sistema_gerenciador_reserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciadorReservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciadorReservaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pa1/sistema_gerenciador_reserva/SistemaGerenciadorReservaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pa1.sistema_gerenciador_reserva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciadorReservaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
